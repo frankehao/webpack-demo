58133b8c78c5abc0acc21df167c1e3f4fca649e7
hao <2524129018@qq.com>
2022-03-23 15:36:21 +0800

    123

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '赵文浩',
+      template: 'src/assets/test.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 }
