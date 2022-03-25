c010af0f23eb4e46f2f9535d75e4c3dca8464baa
hao <2524129018@qq.com>
2022-03-25 13:05:42 +0800

    uodate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './x.js'
 import png from './assets/1.png'
 const div = document.getElementById('app')
-div.innerHTML = `<img src="
-${png}" alt="" />`
 
 const button = document.createElement('button')
 button.innerHTML = `懒加载`
